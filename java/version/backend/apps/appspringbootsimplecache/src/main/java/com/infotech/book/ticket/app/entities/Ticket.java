9aa2c1f19875071c32092bcef80be85f63901505
Pushkar Chauhan <pushkarchauhan91@Pushkars-MacBook-Air.local>
2021-08-04 08:08:09 +0530

    updated pom.xml

--- java/version/backend/apps/appspringbootsimplecache/src/main/java/com/infotech/book/ticket/app/entities/Ticket.java
+++ java/version/backend/apps/appspringbootsimplecache/src/main/java/com/infotech/book/ticket/app/entities/Ticket.java
@@ -9,8 +9,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name="ticket")
+@Data
 public class Ticket {
 
 	@Id
@@ -33,48 +36,4 @@ public class Ticket {
 	@Column(name="email")
 	private String email;
 	
-	
-	public Integer getTicketId() {
-		return ticketId;
-	}
-	public void setTicketId(Integer ticketId) {
-		this.ticketId = ticketId;
-	}
-	public String getPassengerName() {
-		return passengerName;
-	}
-	public void setPassengerName(String passengerName) {
-		this.passengerName = passengerName;
-	}
-	public Date getBookingDate() {
-		return bookingDate;
-	}
-	public void setBookingDate(Date bookingDate) {
-		this.bookingDate = bookingDate;
-	}
-	public String getSourceStation() {
-		return sourceStation;
-	}
-	public void setSourceStation(String sourceStation) {
-		this.sourceStation = sourceStation;
-	}
-	public String getDestStation() {
-		return destStation;
-	}
-	public void setDestStation(String destStation) {
-		this.destStation = destStation;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	@Override
-	public String toString() {
-		return "Ticket [ticketId=" + ticketId + ", passengerName=" + passengerName + ", bookingDate=" + bookingDate
-				+ ", sourceStation=" + sourceStation + ", destStation=" + destStation + ", email=" + email + "]";
-	}
 }
